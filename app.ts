99c10605f980a78d8df4f6fc84f044818d097d17
Mohammed <m1.almatrafi@gmail.com>
2026-06-24 08:56:11 +0300

    feat: product interface

--- app.ts
+++ app.ts
@@ -4,10 +4,23 @@
     price (number),
     and inStock (boolean). */
 
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  inStock: boolean;
+}
+
 /* Create a type called Category that can only be one of:
     'electronics',
     'books',
     or 'clothing'. */
 
+type Category = "electronics" | "books" | "clothing";
+
 /* Write a function called describeProduct that takes a Product and returns a string like "Laptop costs $999".
 Make sure the parameter and the return value are both typed. */
+
+const describeProduct = (product: Product): string => {
+  return `${product.name} costs $${product.price}`;
+};
