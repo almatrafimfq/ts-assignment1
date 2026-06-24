82c0a668cadfe91888801e938954b5919e6bcb31
Mohammed <m1.almatrafi@gmail.com>
2026-06-24 08:51:12 +0300

    initial commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+/* Create an interface called Product with these fields:
+    id (string),
+    name (string),
+    price (number),
+    and inStock (boolean). */
+
+/* Create a type called Category that can only be one of:
+    'electronics',
+    'books',
+    or 'clothing'. */
+
+/* Write a function called describeProduct that takes a Product and returns a string like "Laptop costs $999".
+Make sure the parameter and the return value are both typed. */
